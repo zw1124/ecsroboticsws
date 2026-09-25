5754f6fc32efb0a609e62d940fb5f4d2a3af642e
Kai Jin <zhiliao000@gmail.com>
2026-09-25 06:50:36 -0400

    Use pure text FRC team wordmark on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
       </Reveal>
       <Reveal className="flex flex-col items-center text-center" delay={140}>
         <Image src="/assets/robotecs-eagle-cutout.png" alt="RobotECS eagle logo" width={600} height={600} priority className="w-full max-w-[460px]" />
-        <Image src="/assets/frc-team-12394-wordmark.svg" alt="FRC Team 12394" width={1000} height={220} className="-mt-5 w-full max-w-[460px] md:-mt-7" />
+        <Image src="/assets/frc-team-12394-wordmark.svg" alt="FRC Team 12394" width={1000} height={165} className="-mt-5 w-full max-w-[460px] md:-mt-7" />
       </Reveal>
     </section>
     <section className="grid min-h-[72svh] place-items-center px-5 py-24">
